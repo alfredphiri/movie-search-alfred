521440ff3cfe845dd0d7b5649849ce0cd30d0f7c
alfred <alfredjbphiri@gmail.com>
2025-10-23 10:38:18 +0200

    fix: update button hover effects and styling

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ function App() {
         </div>
         <div className="details">
           <h1 className="hero-title">Find Movies That Suit You</h1>
-          <a href="#" className="watchMoreBtn">Watch More <i class="fa-solid fa-arrow-right"></i></a>
+          <a href="#" className="watchMoreBtn">Watch More <i class="fa-solid fa-arrow-right arrow"></i></a>
         </div>
       </div>
 
